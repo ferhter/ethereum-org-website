11278a7c7a3b518c3e7263ff463e1e2b2673137c
ferhter <126424068+ferhter@users.noreply.github.com>
2023-03-28 19:27:19 -0600

    Create dns.js

---
+++ dns.js
@@ -0,0 +1,12 @@
+Europe / Netherlands / Amsterdam
+ns41.cloudns.net
+ns41.cloudns.net. support.cloudns.net. 2023032901 7200 1800 1209600 3600
+North America / USA / VA
+ns42.cloudns.net
+ns41.cloudns.net. support.cloudns.net. 2023032901 7200 1800 1209600 3600
+Europe / France / Paris
+ns43.cloudns.net
+ns41.cloudns.net. support.cloudns.net. 2023032901 7200 1800 1209600 3600
+Europe / Germany / Frankfurt
+ns44.cloudns.net
+ns41.cloudns.net. support.cloudns.net. 2023032901 7200 1800 1209600 3600
